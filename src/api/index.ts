5924ae2f35b7a831b7d8b267bf242d336bca9410
Georges Markosian <gm@admins-MacBook-Pro.local>
2021-12-21 00:51:15 +0300

    Implement mapping at the API level

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,25 @@
 import * as vendorAPI from './api'
-import * as I from '../DTO'
+import * as I from '../types'
 
 const API = {
   get: {
     employees: vendorAPI.getEmployees as () => Promise<I.Employee[]>,
-    workLogs: vendorAPI.getWorklog as () => Promise<I.WorkLog[]>,
-  }
+    workLogs: () =>
+      vendorAPI.getWorklog().then((logs: I.WorkLogItemDTO[]) =>
+        logs.map(
+          (log): I.WorkLogItem => ({
+            id: log.id,
+            employeeId: log.employee_id,
+            from: Date.parse(log.from),
+            to: Date.parse(log.to),
+            formattedLogRange: {
+              from: log.from,
+              to: log.to,
+            },
+          })
+        )
+      ),
+  },
 }
 
 export default API
